873a4145570e47a6927c566a3d504818ea767895
Khaled Ghareeb <khaledghareeb147@gmail.com>
2026-08-31 13:00:16 +0300

    Fixed SignIn logic and enhanced editdata

--- User.cpp
+++ User.cpp
@@ -4,11 +4,12 @@ set<string> User::usernames;
 set<string> User::passwords;
 unordered_map<string, User*> User::users;
 // NOTE !!!! WE NEED TO HANDLE THAT THE USER INPUTS A VALID USERNAME BEFORE WE INITIALIZE THE OBJECT
-User::User(string firstName, string lastName, string userName, string password) {
+User::User(string firstName, string lastName, string userName, string password, bool student) {
     FirstName = firstName;
     LastName = lastName;
     UserName = userName;
     Password = password;
+    isStudent = student;
     usernames.insert(UserName);
     passwords.insert(Password);
     users[userName] = this;
@@ -31,85 +32,89 @@ void User::SignOut(User*& u) {
     u = nullptr;
 }
 void User::EditData() {
-    string arr[4] = {"firstName", "lastName", "userName", "password"};
-    for(int i = 0; i < 4; i++) {
-        char c;
-        cout << "Do you want to change your " << arr[i] << " ? y/n" << "\n";
-        cin >> c;
-        if(c == 'y' || c == 'Y') {
-            if(arr[i] == "userName" || arr[i] == "password") {
-                cout << "Enter your new " << arr[i] << "\n";
-                string s; 
+    cout << "Choose the data you wish to edit :\n";
+    cout << "1. First Name\n";
+    cout << "2. Last Name\n";
+    cout << "3. UserName\n";
+    cout << "4. Password\n";
+    int choice;
+    cin >> choice;
+    if(choice == 1) {
+        cout << "Enter your new first name : ";
+        string s;
+        cin >> s;
+        cout << "\n";
+        FirstName = s;
+    } else if(choice == 2) {
+        cout << "Enter your new last name\n";
+        string s; 
+        cin >> s;
+        cout << "\n";
+        LastName = s;
+    } else if(choice == 3) {
+        cout << "Enter your new username\n";
+        string s; 
+        cin >> s; 
+        cout << "\n";      
+        if(availableUsername(s)) {
+           usernames.erase(UserName);
+           users.erase(UserName);
+           UserName = s;
+           users[s] = this;
+           usernames.insert(UserName);
+           cout << "Updated\n";
+        } else {
+            char cc;
+            while(!availableUsername(s)) {
+                cout << "Unavailable Username, would you like to enter again? y/n \n";
+                cin >> cc;
+                if(cc == 'N' || cc == 'n') {
+                    break;
+                }
                 cin >> s;
-                if(arr[i] == "userName" ) {
-                    if(availableUsername(s)) {
-                        usernames.erase(UserName);
-                        users.erase(UserName);
-                        UserName = s;
-                        users[s] = this;
-                        usernames.insert(UserName);
-                        cout << "Updated\n";
-                    }
-                    else {
-                        char cc;
-                        while(!availableUsername(s)) {
-                            cout << "Unavailable Username, would you like to enter again? y/n \n";
-                            cin >> cc;
-                            if(cc == 'N' || cc == 'n') {
-                                break;
-                            }
-                            cin >> s;
-                        }
-                        if(availableUsername(s)) {
-                           usernames.erase(UserName);
-                           users.erase(UserName);
-                           UserName = s;
-                           users[s] = this;
-                           usernames.insert(UserName);
-                           cout << "Updated\n";
-                        }
-                    }
-                }
-                if(arr[i] == "password" ) {
-                    if(availablePass(s)) {
-                       passwords.erase(Password);
-                       Password = s;
-                       passwords.insert(Password);
-                       cout << "Updated\n";
-                    }
-                    else {
-                        char cc;
-                        while(!availablePass(s)) {
-                            cout << "Unavailable Password, would you like to enter again? y/n \n";
-                            cin >> cc;
-                            if(cc == 'N' || cc == 'n') {
-                                break;
-                            }
-                            cin >> s;
-                        }
-                       if(availablePass(s)) {
-                          passwords.erase(Password);
-                          Password = s;
-                          passwords.insert(Password);
-                          cout << "Updated\n";
-                        }
-                    }
-                }
-            } else {
-                if(arr[i] == "firstName") {
-                    string s;
-                    cin >> s;
-                    FirstName = s;
-                } else if(arr[i] == "lastName") {
-                    string s; cin >> s;
-                    LastName = s;
-                }
-
+            }
+            if(availableUsername(s)) {
+               usernames.erase(UserName);
+               users.erase(UserName);
+               UserName = s;
+               users[s] = this;
+               usernames.insert(UserName);
+               cout << "Updated\n";
             }
         }
-
+    } else if(choice == 4) {
+        cout << "Enter your new password\n";
+        string s; 
+        cin >> s;
+        cout << "\n";
+        if(availablePass(s)) {
+           passwords.erase(Password);
+           Password = s;
+           passwords.insert(Password);
+           cout << "Updated\n";
+        } else {
+           char cc;
+           while(!availablePass(s)) {
+               cout << "Unavailable Password, would you like to enter again? y/n \n";
+               cin >> cc;
+               if(cc == 'N' || cc == 'n') {
+                   break;
+               }
+               cin >> s;
+           }
+           if(availablePass(s)) {
+              passwords.erase(Password);
+              Password = s;
+              passwords.insert(Password);
+              cout << "Updated\n";
+            }
+        }
+    } else {
+        cout << "INVALID\n";
     }
 }
+
+
 bool User::availableUsername(string username)
 {
     return usernames.find(username) == usernames.end();
--- User.h
+++ User.h
@@ -8,11 +8,12 @@ class User {
   string LastName;
   string UserName;
   string Password;
+  bool isStudent;
   static set<string>usernames;
   static set<string>passwords;
   static unordered_map<string, User*> users;
  public : 
-  User(string firstName, string lastName, string userName, string password);
+  User(string firstName, string lastName, string userName, string password, bool);
   void EditData(); // done
   User* SignIn(string, string); // done
   void SignOut(User*&); // We could make it in the main as a regular function
